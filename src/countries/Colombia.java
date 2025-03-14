de0b686f317aff7549be745166ed26254bed7670
Ruben <ruben.mte@gmail.com>
2025-03-14 10:00:00 +0100

    generador NIT Colombia

--- src/countries/Colombia.java
+++ src/countries/Colombia.java
@@ -1,7 +1,26 @@
 package countries;
 
+import java.util.Random;
+
 public class Colombia {
 
     public Colombia() {}
 
+    public static String generarNIT() {
+        Random random = new Random();
+        // Primeros dos dígitos del NIT son fijos para Colombia (sin considerar ceros a la izquierda)
+        int primerDigito = 9;
+        int segundoDigito = 0;
+
+        // Los siguientes 6 dígitos son aleatorios (sin considerar ceros a la izquierda)
+        int siguiente6Digitos = random.nextInt(899999) + 100000;
+
+        // Último dígito del NIT (dígito de verificación)
+        int digitoVerificacion = generarDigitoVerificacion(primerDigito, segundoDigito, siguiente6Digitos);
+
+        // Construyendo el NIT completo como una cadena de texto
+        String nitCompleto = String.format("%d%d%d-%d", primerDigito, segundoDigito, siguiente6Digitos, digitoVerificacion);
+
+        return nitCompleto;
+    }
 }
