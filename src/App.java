d1c8abbedd9f319b44c4197e1c1773acec3b90da
Ruben <ruben.mte@gmail.com>
2025-03-28 10:00:00 +0100

    Eliminacion del metodo generarDNI de la clase app

--- src/App.java
+++ src/App.java
@@ -47,13 +47,4 @@ public class App {
 
         return digitoVerificacion;
     }
-
-    public static String generarDNI() {
-        final char[] LETRAS_DNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
-                                   'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-        Random random = new Random();
-        int number = random.nextInt(100000000);
-        char letter = LETRAS_DNI[number % 23];
-        return String.format("%08d%c", number, letter);
-    }
 }
