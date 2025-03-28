fc5ea4058ed3a3bdb2c6dc96aa7c632af9d67c33
Ruben <ruben.mte@gmail.com>
2025-03-28 10:00:00 +0100

    Constructor de la clase Spain

--- src/countries/Spain.java
+++ src/countries/Spain.java
@@ -4,6 +4,10 @@ import java.util.Random;
 
 public class Spain {
 
+    public Spain(){
+
+    }
+
     public static String generarDNI() {
         final char[] LETRAS_DNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
                 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
