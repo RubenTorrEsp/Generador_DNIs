cf61675ad04875d2a0b570928e189db540cc5255
Ruben <ruben.mte@gmail.com>
2025-04-01 10:00:00 +0200

    Generador de DNI creado en la clase Spain

--- src/countries/Spain.java
+++ src/countries/Spain.java
@@ -1,4 +1,15 @@
 package countries;
 
+import java.util.Random;
+
 public class Spain {
+
+    public static String generarDNI() {
+        final char[] LETRAS_DNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        Random random = new Random();
+        int number = random.nextInt(100000000);
+        char letter = LETRAS_DNI[number % 23];
+        return String.format("%08d%c", number, letter);
+    }
 }
