d9a09f22ea1277917a2934cee00186e60359d7e1
Ruben <ruben.mte@gmail.com>
2025-04-21 19:47:00 +0200

    Metodo generador de DNIs completado

--- src/App.java
+++ src/App.java
@@ -49,6 +49,11 @@ public class App {
     }
 
     public static String generarDNI() {
-
+        final char[] LETRAS_DNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        Random random = new Random();
+        int number = random.nextInt(100_000_000);
+        char letter = LETRAS_DNI[number % 23];
+        return String.format("%08d%c", number, letter);
     }
 }
