7af10a312a1e9433d56a1e4f8ce57c98d54bf999
Ruben <ruben.mte@gmail.com>
2025-04-23 08:37:24 +0200

    Arreglo del random

--- src/App.java
+++ src/App.java
@@ -52,7 +52,7 @@ public class App {
         final char[] LETRAS_DNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
                                    'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
         Random random = new Random();
-        int number = random.nextInt(100_000_000);
+        int number = random.nextInt(100000000);
         char letter = LETRAS_DNI[number % 23];
         return String.format("%08d%c", number, letter);
     }
