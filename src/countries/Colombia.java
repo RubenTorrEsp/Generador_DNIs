caa560eb5cc4fcf2338bb987ce497cf446dd19b8
Ruben <ruben.mte@gmail.com>
2025-04-24 10:00:00 +0200

    Creacion del metodo generarDigitoVerificacion en la clase Colombia

--- src/countries/Colombia.java
+++ src/countries/Colombia.java
@@ -23,4 +23,27 @@ public class Colombia {
 
         return nitCompleto;
     }
+
+    public static int generarDigitoVerificacion(int primerDigito, int segundoDigito, int siguiente6Digitos) {
+        // Peso de cada uno de los 8 primeros números en el NIT
+        int[] pesos = { 71, 67, 59, 53, 47, 43, 41, 37 };
+
+        // Cálculo del digito de verificación
+        int suma = primerDigito * pesos[0] + segundoDigito * pesos[1];
+        int numero = siguiente6Digitos;
+        for (int i = 0; i < 6; i++) {
+            suma += (numero % 10) * pesos[i + 2];
+            numero /= 10;
+        }
+
+        int residuo = suma % 11;
+        int digitoVerificacion;
+        if (residuo > 1) {
+            digitoVerificacion = 11 - residuo;
+        } else {
+            digitoVerificacion = residuo;
+        }
+
+        return digitoVerificacion;
+    }
 }
