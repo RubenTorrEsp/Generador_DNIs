e0b3870eadbe557d07acc4da2a2c9e9d158332ad
Ruben <ruben.mte@gmail.com>
2025-04-24 10:00:00 +0200

    Limpieza de la clase main

--- src/App.java
+++ src/App.java
@@ -6,24 +6,6 @@ public class App {
         System.out.println("NIT generado: " + generarNIT());
     }
 
-    public static String generarNIT() {
-        Random random = new Random();
-        // Primeros dos dígitos del NIT son fijos para Colombia (sin considerar ceros a la izquierda)
-        int primerDigito = 9;
-        int segundoDigito = 0;
-
-        // Los siguientes 6 dígitos son aleatorios (sin considerar ceros a la izquierda)
-        int siguiente6Digitos = random.nextInt(899999) + 100000;
-
-        // Último dígito del NIT (dígito de verificación)
-        int digitoVerificacion = generarDigitoVerificacion(primerDigito, segundoDigito, siguiente6Digitos);
-
-        // Construyendo el NIT completo como una cadena de texto
-        String nitCompleto = String.format("%d%d%d-%d", primerDigito, segundoDigito, siguiente6Digitos, digitoVerificacion);
-
-        return nitCompleto;
-    }
-
     public static int generarDigitoVerificacion(int primerDigito, int segundoDigito, int siguiente6Digitos) {
         // Peso de cada uno de los 8 primeros números en el NIT
         int[] pesos = { 71, 67, 59, 53, 47, 43, 41, 37 };
