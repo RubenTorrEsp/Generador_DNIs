74de423a28cf0dbab321f380ec4d7eb42d72c7b6
Ruben <ruben.mte@gmail.com>
2025-04-30 10:00:00 +0200

    Creación del texto países

--- src/resources/Texts.java
+++ src/resources/Texts.java
@@ -1,4 +1,10 @@
 package resources;
 
 public class Texts {
+
+    public String paises = """
+            Elija el país del que quiera generar el documento:
+            Colombia
+            España""";
+
 }
