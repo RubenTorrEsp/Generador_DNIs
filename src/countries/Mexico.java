2fef06349a2246a418c6788afff7521a27170080
Ruben <ruben.mte@gmail.com>
2025-05-08 10:26:13 +0200

    Desarrollo de la clase mexico

--- src/countries/Mexico.java
+++ src/countries/Mexico.java
@@ -6,6 +6,40 @@ import java.util.Random;
 
 public class Mexico {
 
-    public Mexico(){}
+    public static String generarCURP(String nombre, String apPaterno, String apMaterno,
+                                     LocalDate fechaNac, char sexo, String estado) {
+
+        nombre = nombre.toUpperCase();
+        apPaterno = apPaterno.toUpperCase();
+        apMaterno = apMaterno.toUpperCase();
+        estado = estado.toUpperCase();
+
+        String curp = "";
+
+        // Letras del nombre y apellidos
+        curp += apPaterno.charAt(0);                            // 1ª letra apellido paterno
+        curp += primeraVocalInterna(apPaterno);                 // 1ª vocal interna apellido paterno
+        curp += apMaterno.charAt(0);                            // 1ª letra apellido materno
+        curp += nombre.charAt(0);                               // 1ª letra del nombre
+
+        // Fecha de nacimiento
+        curp += fechaNac.format(DateTimeFormatter.ofPattern("yyMMdd"));
+
+        // Sexo
+        curp += sexo;
+
+        // Estado
+        curp += estado;
+
+        // Consonantes internas
+        curp += primeraConsonanteInterna(apPaterno);
+        curp += primeraConsonanteInterna(apMaterno);
+        curp += primeraConsonanteInterna(nombre);
+
+        // Dígitos de control (para homonimia y verificación)
+        curp += String.format("%02d", new Random().nextInt(100)); // Random 2 dígitos
+
+        return curp;
+    }
 
 }
